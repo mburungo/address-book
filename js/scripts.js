42b1fa943d6f08589d348e6bc293c942d9d56337
mburungo <mmburungo@gmail.com>
2018-05-14 20:27:46 +0300

    display contacts

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,20 @@ class Contact {
   }
 }
 $ (document).ready(function()
+//define empty array to hold contact objects
   var allContacts = [];
   $('form#new-contact').submit(function(event)){
     event.preventDefault()
+    //gets values from input fields
     var firstName = $("#first-name").val();
     var lastName = $("#last-name").val ();
     var phoneNumber = $("#phone-number").val ();
+    //create new contact
     var newContact = new Contact (firstName, lastName, phoneNumber)
     allContacts.push(newContact);
+    //clear values from form input fields
+    document.getElementById("new-contact").reset();
     console.log(allContacts);
+    $("ul#contacts").apppend("<li>" "+newContact.lastName+ ", "+newContact.firstName+"</li>)
   });
-
 )};
