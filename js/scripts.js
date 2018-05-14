54c57c69e18699aa7700c33b0b184210e011a39e
mburungo <mmburungo@gmail.com>
2018-05-14 20:07:05 +0300

    adding contacts js

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@ class Contact {
     this.phoneNumber = phoneNumber;
   }
 }
+$ (document).ready(function()
+  var allContacts = [];
+  $('form#new-contact').submit(function(event)){
+    event.preventDefault()
+    var firstName = $("#first-name").val();
+    var lastName = $("#last-name").val ();
+    var phoneNumber = $("#phone-number").val ();
+    var newContact = new Contact (firstName, lastName, phoneNumber)
+    allContacts.push(newContact);
+    console.log(allContacts);
+  });
+
+)};
