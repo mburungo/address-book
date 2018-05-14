b9897e900c103e7a972f2817e65bf0a1017aa96f
mburungo <mmburungo@gmail.com>
2018-05-14 20:47:25 +0300

    final looks

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,31 @@
 class Contact {
-  constructor(firstName, lastName, phoneNumber){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phoneNumber = phoneNumber;
-  }
+    constructor(firstName, lastName, phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
 }
-$ (document).ready(function()
-//define empty array to hold contact objects
-  var allContacts = [];
-  $('form#new-contact').submit(function(event)){
-    event.preventDefault()
-    //gets values from input fields
-    var firstName = $("#first-name").val();
-    var lastName = $("#last-name").val ();
-    var phoneNumber = $("#phone-number").val ();
-    //create new contact
-    var newContact = new Contact (firstName, lastName, phoneNumber)
-    allContacts.push(newContact);
-    //clear values from form input fields
-    document.getElementById("new-contact").reset();
-    console.log(allContacts);
-    $("ul#contacts").apppend("<li>" "+newContact.lastName+ ", "+newContact.firstName+"</li>)
-  });
-)};
+
+$(document).ready(function() {
+    // Define empty array to hold contact objects
+    var allContacts = [];
+
+    $('form#new-contact').submit(function(event) {
+        event.preventDefault();
+        // gets values from input fields
+        var firstName = $("#first-name").val();
+        var lastName = $("#last-name").val();
+        var phoneNumber = $("#phone-number").val();
+
+        // create new contact object
+        var newContact = new Contact(firstName, lastName, phoneNumber);
+        // Add new contact to allContacts array
+        allContacts.push(newContact);
+        // clear values from form input fields
+        $(this).trigger("reset");
+        $("ul#contacts").append(`<li class="contact" id=${allContacts.length - 1}> ${newContact.lastName}, ${newContact.firstName}</li>`);
+    });
+    $("li.contact").click (function(event)){
+      console.log ("Clicked");
+    }
+});
