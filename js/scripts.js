d638ea6907d9389c604063d04b110f7c1e9d3064
mburungo <mmburungo@gmail.com>
2018-05-14 19:42:29 +0300

    add contact class

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-script>
+class Contact {
+  constructor(firstName, lastName, phoneNumber){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+  }
+}
